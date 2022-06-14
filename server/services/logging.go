15ea53ef5e4f0900f851d6ba6b28bef4a8162ad2
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-06-15 00:25:28 +0300

    Add some logging.

--- server/services/logging.go
+++ server/services/logging.go
@@ -1,5 +1,43 @@
 package services
 
-import logger "github.com/ElrondNetwork/elrond-go-logger"
+import (
+	"fmt"
+	"strings"
+
+	logger "github.com/ElrondNetwork/elrond-go-logger"
+	"github.com/coinbase/rosetta-sdk-go/types"
+)
 
 var log = logger.GetOrCreate("server/services")
+
+func traceBlockResponse(response *types.BlockResponse) {
+	if log.GetLevel() > logger.LogTrace {
+		return
+	}
+
+	log.Trace("Block",
+		"nonce", response.Block.BlockIdentifier.Index,
+		"hash", response.Block.BlockIdentifier.Hash,
+		"numTxs", len(response.Block.Transactions),
+	)
+
+	for txIndex, tx := range response.Block.Transactions {
+		fmt.Printf("(TX %d) %s\n", txIndex, tx.TransactionIdentifier.Hash)
+
+		for opIndex, op := range tx.Operations {
+			formattedAmount := formatAmount(op.Amount)
+
+			fmt.Printf("\t(OP %d) %s\t%25s\n", opIndex, op.Account.Address, formattedAmount)
+		}
+	}
+}
+
+func formatAmount(amount *types.Amount) string {
+	value := amount.Value
+	hasSign := strings.HasPrefix(value, "-") || strings.HasPrefix(value, "+")
+	if !hasSign {
+		value = "+" + value
+	}
+
+	return value
+}
