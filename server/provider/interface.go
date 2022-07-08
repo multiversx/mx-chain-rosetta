128925556374feaed01a595b3554ecfacef23211
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-07-08 17:43:21 +0300

    Use "smaller" interfaces.

--- server/provider/interface.go
+++ server/provider/interface.go
@@ -1,22 +1,26 @@
 package provider
 
 import (
-	"github.com/ElrondNetwork/elrond-proxy-go/facade"
-	"github.com/ElrondNetwork/elrond-proxy-go/process"
+	"github.com/ElrondNetwork/elrond-proxy-go/common"
+	"github.com/ElrondNetwork/elrond-proxy-go/data"
 )
 
 type baseProcessor interface {
-	process.Processor
+	CallGetRestEndPoint(address string, path string, value interface{}) (int, error)
+	ComputeShardId(addressBuff []byte) (uint32, error)
 }
 
 type accountProcessor interface {
-	facade.AccountProcessor
+	GetAccount(address string, options common.AccountQueryOptions) (*data.AccountModel, error)
 }
 
 type transactionProcessor interface {
-	facade.TransactionProcessor
+	SendTransaction(tx *data.Transaction) (int, string, error)
+	ComputeTransactionHash(tx *data.Transaction) (string, error)
+	GetTransactionByHashAndSenderAddress(txHash string, sndAddr string, withEvents bool) (*data.FullTransaction, int, error)
 }
 
 type blockProcessor interface {
-	facade.BlockProcessor
+	GetBlockByHash(shardID uint32, hash string, options common.BlockQueryOptions) (*data.BlockApiResponse, error)
+	GetBlockByNonce(shardID uint32, nonce uint64, options common.BlockQueryOptions) (*data.BlockApiResponse, error)
 }
