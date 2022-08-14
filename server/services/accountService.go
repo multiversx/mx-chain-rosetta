790133e77e76efb34b9756740e9a070d3dcaab04
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-08-14 23:28:16 +0300

    Fix AccountBalance().

--- server/services/accountService.go
+++ server/services/accountService.go
@@ -33,12 +33,25 @@ func (service *accountService) AccountBalance(
 		return nil, service.errFactory.newErr(ErrInvalidAccountAddress)
 	}
 
-	isForOneCurrency := len(request.Currencies) == 1
-	if !isForOneCurrency {
+	// The specification states:
+	// > If the currencies field is populated, only balances for the specified currencies will be returned.
+	// > If not populated, all available balances will be returned.
+	// https://www.rosetta-api.org/docs/models/AccountBalanceRequest.html
+
+	// However, we cannot fully implement this requirement at the moment.
+	var currencySymbol string
+
+	if len(request.Currencies) == 0 {
+		// For the moment, we only return the native currency when "request.Currencies" is empty.
+		currencySymbol = service.getNativeSymbol()
+	} else if len(request.Currencies) == 1 {
+		currencySymbol = request.Currencies[0].Symbol
+	} else {
+		// For the moment, we cannot atomically fetch multiple currencies at "the latest final block",
+		// thus we postpone the implementation of this feature (doable in a future PR).
 		return nil, service.errFactory.newErr(ErrNotImplemented)
 	}
 
-	currencySymbol := request.Currencies[0].Symbol
 	amount, blockCoordinates, err := service.getBalance(address, currencySymbol)
 	if err != nil {
 		return nil, service.errFactory.newErrWithOriginal(ErrUnableToGetAccount, err)
@@ -53,7 +66,7 @@ func (service *accountService) AccountBalance(
 }
 
 func (service *accountService) getBalance(address string, currencySymbol string) (*types.Amount, resources.AccountBlockCoordinates, error) {
-	isForNative := currencySymbol == service.provider.GetNativeCurrency().Symbol
+	isForNative := currencySymbol == service.getNativeSymbol()
 	if isForNative {
 		accountBalance, err := service.provider.GetAccountNativeBalance(address)
 		if err != nil {
@@ -71,6 +84,10 @@ func (service *accountService) getBalance(address string, currencySymbol string)
 
 	amount := service.extension.valueToCustomAmount(accountBalance.Balance, currencySymbol)
 	return amount, accountBalance.BlockCoordinates, nil
+}
+
+func (service *accountService) getNativeSymbol() string {
+	return service.provider.GetNativeCurrency().Symbol
 }
 
 // AccountCoins implements the /account/coins endpoint.
