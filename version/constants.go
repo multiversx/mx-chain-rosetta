39ec0e6703e78edfe8c7222121b9dfe555723bc5
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-08-25 16:48:14 +0300

    Bump version.

--- version/constants.go
+++ version/constants.go
@@ -5,7 +5,7 @@ const (
 	RosettaVersion = "v1.4.12"
 
 	// RosettaMiddlewareVersion is the version of this package (application)
-	RosettaMiddlewareVersion = "v0.2.0"
+	RosettaMiddlewareVersion = "v0.2.1"
 
 	// NodeVersion is the canonical version of the node runtime
 	NodeVersion = "rc/2022-july"
