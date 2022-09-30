fc632d703516fac29f752387ebd34b7df219126a
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-09-30 18:25:53 +0300

    Discard operations with value "-0".

--- server/services/common.go
+++ server/services/common.go
@@ -3,7 +3,7 @@ package services
 type objectsMap map[string]interface{}
 
 func isZeroAmount(amount string) bool {
-	return amount == "" || amount == "0"
+	return amount == "" || amount == "0" || amount == "-0"
 }
 
 func isNonZeroAmount(amount string) bool {
