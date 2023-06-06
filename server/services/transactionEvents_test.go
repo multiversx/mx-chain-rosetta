3c4edad8cfbceced06ac5bf48028330eed9c0e5f
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-06-06 17:38:12 +0300

    Add some unit tests.

---
+++ server/services/transactionEvents_test.go
@@ -0,0 +1,46 @@
+package services
+
+import (
+	"bytes"
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEventESDT(t *testing.T) {
+	t.Run("without nonce (fungible)", func(t *testing.T) {
+		event := eventESDT{
+			identifier:   "FOO-abcdef",
+			nonceAsBytes: nil,
+		}
+
+		require.Equal(t, "FOO-abcdef", event.getBaseIdentifier())
+		require.Equal(t, "FOO-abcdef", event.getComposedIdentifier())
+
+		event = eventESDT{
+			identifier:   "FOO-abcdef",
+			nonceAsBytes: numberToBytesWithoutLeadingZeros(0),
+		}
+
+		require.Equal(t, "FOO-abcdef", event.getBaseIdentifier())
+		require.Equal(t, "FOO-abcdef", event.getComposedIdentifier())
+	})
+
+	t.Run("with nonce (SFT, MetaESDT, NFT)", func(t *testing.T) {
+		event := eventESDT{
+			identifier:   "FOO-abcdef",
+			nonceAsBytes: numberToBytesWithoutLeadingZeros(42),
+		}
+
+		require.Equal(t, "FOO-abcdef", event.getBaseIdentifier())
+		require.Equal(t, "FOO-abcdef-2a", event.getComposedIdentifier())
+	})
+}
+
+func numberToBytesWithoutLeadingZeros(number uint64) []byte {
+	data := make([]byte, 8)
+	binary.BigEndian.PutUint64(data, number)
+	data = bytes.Trim(data, "\x00")
+	return data
+}
