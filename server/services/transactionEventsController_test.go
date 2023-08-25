6a134abeb0ed76d1651234484fd05280643e9e10
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-08-25 16:03:44 +0300

    Implement "transactionEventsController.hasAnySignalError()".

--- server/services/transactionEventsController_test.go
+++ server/services/transactionEventsController_test.go
@@ -8,6 +8,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTransactionEventsController_HasAnySignalError(t *testing.T) {
+	networkProvider := testscommon.NewNetworkProviderMock()
+	controller := newTransactionEventsController(networkProvider)
+
+	t.Run("arbitrary tx", func(t *testing.T) {
+		tx := &transaction.ApiTransactionResult{}
+		txMatches := controller.hasAnySignalError(tx)
+		require.False(t, txMatches)
+	})
+
+	t.Run("tx with event 'signalError'", func(t *testing.T) {
+		tx := &transaction.ApiTransactionResult{
+			Logs: &transaction.ApiLogs{
+
+				Events: []*transaction.Events{
+					{
+						Identifier: transactionEventSignalError,
+					},
+				},
+			},
+		}
+
+		txMatches := controller.hasAnySignalError(tx)
+		require.True(t, txMatches)
+	})
+}
+
 func TestTransactionEventsController_HasSignalErrorOfSendingValueToNonPayableContract(t *testing.T) {
 	networkProvider := testscommon.NewNetworkProviderMock()
 	controller := newTransactionEventsController(networkProvider)
@@ -83,6 +110,18 @@ func TestTransactionEventsController_HasSignalErrorOfMetaTransactionIsInvalid(t 
 		txMatches := controller.hasSignalErrorOfMetaTransactionIsInvalid(tx)
 		require.True(t, txMatches)
 	})
+}
+
+func Test(t *testing.T) {
+	event := transaction.Events{
+		Identifier: transactionEventSignalError,
+		Topics: [][]byte{
+			[]byte("foo"),
+		},
+	}
+
+	require.True(t, eventHasTopic(&event, "foo"))
+	require.False(t, eventHasTopic(&event, "bar"))
 }
 
 func TestEventHasTopic(t *testing.T) {
