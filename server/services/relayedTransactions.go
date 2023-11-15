256731734facc154cc863b1196d4204a8b41f0ca
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-11-15 14:36:52 +0200

    Handle relayed V2 (in progress).

--- server/services/relayedTransactions.go
+++ server/services/relayedTransactions.go
@@ -11,16 +11,26 @@ import (
 
 // innerTransactionOfRelayedV1 is used to parse the inner transaction of a relayed V1 transaction, and holds only the fields handled by Rosetta.
 type innerTransactionOfRelayedV1 struct {
-	Nonce          uint64  `json:"nonce"`
 	Value          big.Int `json:"value"`
 	ReceiverPubKey []byte  `json:"receiver"`
 	SenderPubKey   []byte  `json:"sender"`
 }
 
+// innerTransactionOfRelayedV2 is used to parse the inner transaction of a relayed V2 transaction, and holds only the fields handled by Rosetta.
+type innerTransactionOfRelayedV2 struct {
+	ReceiverPubKey []byte `json:"receiver"`
+}
+
 func isRelayedV1Transaction(tx *transaction.ApiTransactionResult) bool {
 	return (tx.Type == string(transaction.TxTypeNormal)) &&
-		(tx.ProcessingTypeOnSource == transactionProcessingTypeRelayed) &&
-		(tx.ProcessingTypeOnDestination == transactionProcessingTypeRelayed)
+		(tx.ProcessingTypeOnSource == transactionProcessingTypeRelayedV1) &&
+		(tx.ProcessingTypeOnDestination == transactionProcessingTypeRelayedV1)
+}
+
+func isRelayedV2Transaction(tx *transaction.ApiTransactionResult) bool {
+	return (tx.Type == string(transaction.TxTypeNormal)) &&
+		(tx.ProcessingTypeOnSource == transactionProcessingTypeRelayedV2) &&
+		(tx.ProcessingTypeOnDestination == transactionProcessingTypeRelayedV2)
 }
 
 func parseInnerTxOfRelayedV1(tx *transaction.ApiTransactionResult) (*innerTransactionOfRelayedV1, error) {
@@ -42,4 +52,20 @@ func parseInnerTxOfRelayedV1(tx *transaction.ApiTransactionResult) (*innerTransa
 	}
 
 	return &innerTx, nil
+}
+
+func parseInnerTxOfRelayedV2(tx *transaction.ApiTransactionResult) (*innerTransactionOfRelayedV2, error) {
+	subparts := strings.Split(string(tx.Data), argumentsSeparator)
+	if len(subparts) != 5 {
+		return nil, errCannotParseRelayedV2
+	}
+
+	receiverPubKey, err := hex.DecodeString(subparts[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return &innerTransactionOfRelayedV2{
+		ReceiverPubKey: receiverPubKey,
+	}, nil
 }
