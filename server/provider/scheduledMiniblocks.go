7ab79e31ec3a666700c56f964b8571c9337b25a9
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-08-27 12:17:21 +0300

    Additional explanations, minor refactoring.

--- server/provider/scheduledMiniblocks.go
+++ server/provider/scheduledMiniblocks.go
@@ -23,6 +23,7 @@ func (provider *networkProvider) simplifyBlockWithScheduledTransactions(block *a
 	return nil
 }
 
+// reportScheduledTransactions logs the number of transactions in miniblocks of types: "Scheduled", "Processed", and "Invalid".
 func reportScheduledTransactions(block *api.Block) {
 	numScheduled := 0
 	numProcessed := 0
@@ -47,18 +48,22 @@ func doSimplifyBlockWithScheduledTransactions(previousBlock *api.Block, block *a
 	txs := gatherEffectiveTransactions(block.Shard, previousBlock, block, nextBlock)
 	receipts := gatherAllReceipts(block)
 
+	// Downstream, when recovering balance-changing operations, we do not care about the original container (miniblock) of the effective transaction.
+	// We group all transactions in an "artificial" miniblock, and all receipts in another "artificial" miniblock.
 	block.MiniBlocks = []*api.MiniBlock{
 		{
-			Type:         "Artificial",
+			Type:         miniblockTypeArtificial,
 			Transactions: txs,
 		},
 		{
-			Type:     "Artificial",
+			Type:     miniblockTypeArtificial,
 			Receipts: receipts,
 		},
 	}
 }
 
+// gatherEffectiveTransactions gathers transactions whose effects (mutation of accounts state) are visible in the current block.
+// They are gathered from the previous, current and next block.
 func gatherEffectiveTransactions(selfShard uint32, previousBlock *api.Block, currentBlock *api.Block, nextBlock *api.Block) []*transaction.ApiTransactionResult {
 	txsInCurrentBlock := gatherAllTransactions(currentBlock)
 
@@ -66,6 +71,7 @@ func gatherEffectiveTransactions(selfShard uint32, previousBlock *api.Block, cur
 	scheduledTxsInCurrentBlock := gatherScheduledTransactions(currentBlock)
 
 	if len(scheduledTxsInPreviousBlock) == 0 && len(scheduledTxsInCurrentBlock) == 0 {
+		// Trivial case, no special handling needed.
 		return txsInCurrentBlock
 	}
 
@@ -73,22 +79,24 @@ func gatherEffectiveTransactions(selfShard uint32, previousBlock *api.Block, cur
 	var currentlyExecutedResults []*transaction.ApiTransactionResult
 
 	if len(scheduledTxsInPreviousBlock) > 0 {
+		// Look behind, for any contract results that, even if present in the current block, had their effects in the previous block.
 		previouslyExecutedResults = findImmediatelyExecutingContractResults(selfShard, scheduledTxsInPreviousBlock, txsInCurrentBlock)
 	}
 	if len(scheduledTxsInCurrentBlock) > 0 {
+		// Look ahead, for any contract results that, even if present in the next block, have their effects in the current block.
 		txsInNextBlock := gatherAllTransactions(nextBlock)
 		currentlyExecutedResults = findImmediatelyExecutingContractResults(selfShard, scheduledTxsInCurrentBlock, txsInNextBlock)
 	}
 
 	// effectiveTxs
-	//	= txsInCurrentBlock
-	//	- txsInPreviousBlock (excludes transactions in "processed" miniblocks, for example)
-	//	- previouslyExecutedResults
-	//	+ currentlyExecutedResults
+	//	= txsInCurrentBlock																		term (a)
+	//	- txsInPreviousBlock (excludes transactions in "processed" miniblocks, for example)		term (b)
+	//	- previouslyExecutedResults																term (c)
+	//	+ currentlyExecutedResults																term (d)
 
-	effectiveTxs := make([]*transaction.ApiTransactionResult, 0)
 	effectiveTxsByHash := make(map[string]*transaction.ApiTransactionResult)
 
+	// term (a)
 	for _, tx := range txsInCurrentBlock {
 		effectiveTxsByHash[tx.Hash] = tx
 	}
@@ -96,20 +104,26 @@ func gatherEffectiveTransactions(selfShard uint32, previousBlock *api.Block, cur
 	if len(scheduledTxsInPreviousBlock) > 0 {
 		txsInPreviousBlock := gatherAllTransactions(previousBlock)
 
+		// term (b)
 		for _, tx := range txsInPreviousBlock {
 			delete(effectiveTxsByHash, tx.Hash)
 		}
 
+		// term (c)
 		for _, tx := range previouslyExecutedResults {
 			delete(effectiveTxsByHash, tx.Hash)
 		}
 	}
 
 	if len(scheduledTxsInCurrentBlock) > 0 {
+		// term (d)
 		for _, tx := range currentlyExecutedResults {
 			effectiveTxsByHash[tx.Hash] = tx
 		}
 	}
+
+	// Collect the effective transactions in a slice.
+	effectiveTxs := make([]*transaction.ApiTransactionResult, 0, len(effectiveTxsByHash))
 
 	for _, tx := range effectiveTxsByHash {
 		effectiveTxs = append(effectiveTxs, tx)
@@ -123,7 +137,7 @@ func findImmediatelyExecutingContractResults(
 	transactions []*transaction.ApiTransactionResult,
 	maybeContractResults []*transaction.ApiTransactionResult,
 ) []*transaction.ApiTransactionResult {
-	immediateleyExecutingContractResults := make([]*transaction.ApiTransactionResult, 0)
+	immediatelyExecutingContractResults := make([]*transaction.ApiTransactionResult, 0)
 	nextContractResultsByHash := make(map[string][]*transaction.ApiTransactionResult)
 
 	for _, item := range maybeContractResults {
@@ -131,11 +145,11 @@ func findImmediatelyExecutingContractResults(
 	}
 
 	for _, tx := range transactions {
-		immediateleyExecutingContractResultsPart := findImmediatelyExecutingContractResultsOfTransaction(selfShard, tx, nextContractResultsByHash)
-		immediateleyExecutingContractResults = append(immediateleyExecutingContractResults, immediateleyExecutingContractResultsPart...)
+		immediatelyExecutingContractResultsPart := findImmediatelyExecutingContractResultsOfTransaction(selfShard, tx, nextContractResultsByHash)
+		immediatelyExecutingContractResults = append(immediatelyExecutingContractResults, immediatelyExecutingContractResultsPart...)
 	}
 
-	return immediateleyExecutingContractResults
+	return immediatelyExecutingContractResults
 }
 
 func findImmediatelyExecutingContractResultsOfTransaction(
@@ -146,8 +160,8 @@ func findImmediatelyExecutingContractResultsOfTransaction(
 	immediatelyExecutingContractResults := make([]*transaction.ApiTransactionResult, 0)
 
 	for _, nextContractResult := range nextContractResultsByHash[tx.Hash] {
-		// Not immediately executing.
 		if nextContractResult.SourceShard != selfShard {
+			// Cross-shard, not immediately executing.
 			continue
 		}
 
@@ -160,6 +174,7 @@ func findImmediatelyExecutingContractResultsOfTransaction(
 	return immediatelyExecutingContractResults
 }
 
+// gatherScheduledTransactions gathers transactions in miniblocks of type "Scheduled"
 func gatherScheduledTransactions(block *api.Block) []*transaction.ApiTransactionResult {
 	scheduledTxs := make([]*transaction.ApiTransactionResult, 0)
 
@@ -177,6 +192,7 @@ func gatherScheduledTransactions(block *api.Block) []*transaction.ApiTransaction
 	return scheduledTxs
 }
 
+// gatherAllTransactions gathers all transactions, from all miniblocks
 func gatherAllTransactions(block *api.Block) []*transaction.ApiTransactionResult {
 	txs := make([]*transaction.ApiTransactionResult, 0)
 
@@ -189,6 +205,7 @@ func gatherAllTransactions(block *api.Block) []*transaction.ApiTransactionResult
 	return txs
 }
 
+// gatherAllReceipts gathers all receipts, from all miniblocks
 func gatherAllReceipts(block *api.Block) []*transaction.ApiReceipt {
 	receipts := make([]*transaction.ApiReceipt, 0)
 
